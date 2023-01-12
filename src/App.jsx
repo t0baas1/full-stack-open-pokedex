45414605beb631af2c9bab3c2f906fa9d5bb2f61
Topi Kanninen <topi.kanninen@sci.fi>
2023-01-12 11:33:18 +0200

    broken update

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,13 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
 })))
 
 const App = () => {
-  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
+/*   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
   if (isLoading) {
     return <LoadingSpinner />
   }
   if (error) {
     return <ErrorMessage error={error} />
-  }
+  } */
 
   return (
     <Router>
